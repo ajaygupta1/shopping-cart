c10149d1b4627454827a3b09e340541c21a316a8
ajaygupta <ajaygupta1@gmail.com>
2018-03-19 16:49:21 +0000

    Shopping cart implementaiton for offer

--- src/main/scala/com/myshop/checkout/ShoppingCartCheckout.scala
+++ src/main/scala/com/myshop/checkout/ShoppingCartCheckout.scala
@@ -19,5 +19,32 @@ class ShoppingCartCheckout {
       case _ => throw new IllegalArgumentException("We dont sell this item")
     }).sum
   }
+
+
+  def scanItemWithOffer(items: Array[String]): BigDecimal = {
+
+    var appleCount = 0
+    var orangeCount = 0
+
+    items.map({
+      case apple.name => {
+        appleCount += 1
+        if(appleCount%2  == 0){
+          BigDecimal(0)
+        } else{
+          apple.price
+        }
+      }
+      case orange.name => {
+        orangeCount += 1
+        if(orangeCount%3  == 0){
+          BigDecimal(0)
+        } else{
+          orange.price
+        }
+      }
+      case _ => throw new IllegalArgumentException("We dont sell this item")
+    }).sum
+  }
 }
 
--- src/test/scala/com/myshop/checkout/ShoppingCartCheckoutTestSpec.scala
+++ src/test/scala/com/myshop/checkout/ShoppingCartCheckoutTestSpec.scala
@@ -30,6 +30,37 @@ class ShoppingCartCheckoutTestSpec extends  FlatSpec with Matchers with OptionVa
       a [IllegalArgumentException] should be thrownBy(checkout scanItem Array("Pineapple"))
   }
 
+  // Tests for scan with offer
+  "A Shopping cart Checkout With Offer" should "Return the total cost of 1-Apple and 2-Orange Only when 2-Apple and 3 Orange are present in the cart" in {
+    checkout scanItemWithOffer Array("Orange", "Apple", "Apple", "Orange", "Orange") should be (BigDecimal("1.10"))
+  }
+
+  it should "Apply offer only on Apple when cart has 2-Apple and 2-Orange" in {
+    checkout scanItemWithOffer Array("Orange", "Apple", "Apple", "Orange") should be (BigDecimal("1.10"))
+  }
+
+  it should "Apply offer only on Orange when cart has 1-Apple and 3-Orange" in {
+    checkout scanItemWithOffer Array("Orange", "Apple", "Orange", "Orange") should be (BigDecimal("1.10"))
+  }
+
+  it should "Apply offer on both Apple and Orange when cart has 2-Apple and 3-Orange" in {
+    checkout scanItemWithOffer Array("Orange", "Apple", "Apple", "Apple", "Orange", "Orange") should be (BigDecimal("1.70"))
+  }
+
+  it should "Not apply any offer when only 1-Apple and 2 Orange is present in cart" in {
+    checkout scanItemWithOffer Array("Apple", "Orange", "Orange") should be (BigDecimal("1.10"))
+  }
+
+  it should "Return a zero cost for an empty cart, no offer applied" in {
+    checkout scanItemWithOffer Array() should be (BigDecimal("0"))
+  }
+
+  it should "Throw an IllegalArgumentException for offer based checkout when a Pineapple is scanned which the shop doesnt sell" in {
+    a [IllegalArgumentException] should be thrownBy(checkout scanItemWithOffer Array("Pineapple"))
+  }
+
+
+
 }
 
 
